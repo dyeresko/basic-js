ccca66ebdcc6367176a4f1c9403d653470feb7fc
Denis Yeresko <denis.yeresko@gmail.com>
2025-01-02 19:14:56 +0200

    feat: finish task carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,16 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  const k = 0.693 / HALF_LIFE_PERIOD;
+  const convertedSampleActivity = Number(sampleActivity);
+  if (isNaN(convertedSampleActivity)
+     || typeof sampleActivity !== "string"
+     || +sampleActivity > 15 || sampleActivity.trim() === '' || +sampleActivity <= 0) {
+    return false;
+  } else {
+    return Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) / k);
+  }
 }
 
 module.exports = {
