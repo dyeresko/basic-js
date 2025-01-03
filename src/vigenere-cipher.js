1387b3116bee40865274ee30aa862b15e528bec3
Denis Yeresko <denis.yeresko@gmail.com>
2025-01-03 17:46:00 +0200

    feat: finish task vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,63 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  reverse = true;
+  constructor(r) {
+    this.reverse = r;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(message, key) {
+    if (arguments[0] === undefined || arguments[1] === undefined || arguments.length < 2) {
+      throw new Error("Incorrect arguments!");
+    }
+    let newKey = "";
+    let i = 0;
+    const newMessage = message.toUpperCase();
+    const resultArray = [];
+    const aCharCode = "A".charCodeAt(0);
+    let j = 0;
+    while (newKey.length !== newMessage.length) {
+      newKey += key[i % key.length];
+      i += 1;
+    }
+    newKey = newKey.toUpperCase();
+    for (let i = 0; i < newMessage.length; i += 1) {
+      if ((/[a-zA-Z]/.test(newMessage[i]))) {
+        const shift = newKey.charCodeAt(j) - aCharCode;
+        const newCharacter = String.fromCharCode((newMessage.charCodeAt(i) - aCharCode + shift) % 26 + aCharCode);
+        resultArray.push(newCharacter);
+        j += 1;
+      } else {
+        resultArray.push(newMessage[i]);
+      }
+    }
+    return this.reverse ? resultArray.reverse().join("") : resultArray.join("");
+  }
+  decrypt(message, key) {
+    if (arguments[0] === undefined || arguments[1] === undefined || arguments.length < 2) {
+      throw new Error("Incorrect arguments!");
+    }
+    let newKey = "";
+    let i = 0;
+    const newMessage = message.toUpperCase();
+    const resultArray = [];
+    const aCharCode = "A".charCodeAt(0);
+    let j = 0;
+    while (newKey.length !== newMessage.length) {
+      newKey += key[i % key.length];
+      i += 1;
+    }
+    newKey = newKey.toUpperCase();
+    for (let i = 0; i < newMessage.length; i += 1) {
+      if ((/[a-zA-Z]/.test(newMessage[i]))) {
+        const shift = newKey.charCodeAt(j) - aCharCode;
+        const newCharacter = String.fromCharCode((newMessage.charCodeAt(i) - aCharCode - shift + 26) % 26 + aCharCode);
+        resultArray.push(newCharacter);
+        j += 1;
+      } else {
+        resultArray.push(newMessage[i]);
+      }
+    }
+    return this.reverse ? resultArray.reverse().join("") : resultArray.join("");
   }
 }
 
